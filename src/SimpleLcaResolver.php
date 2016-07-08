7676d10551a9150cfe528933c3c07d74ddbfb727
Rakesh Verma <rkv160794@gmail.com>
2016-07-08 23:18:38 +0530

    updated code for simple LCA

--- src/SimpleLcaResolver.php
+++ src/SimpleLcaResolver.php
@@ -2,16 +2,22 @@
 
 namespace Drupal\conflict;
 
+use Drupal\Core\Entity\RevisionableInterface;
+
 class SimpleLcaResolver implements ConflictAncestorResolverInterface
 {
-    public function resolve($revision1, $revision2) {
-      if ($revision1 < $revision2) {
-            return $revision1-1;
-        }
-        return $revision2-1;
-    }
     public function applies()
     {
         return True;
     }
+
+    public function resolve(RevisionableInterface $revision1,RevisionableInterface $revision2) {
+        $revid1 = $revision1->getRevisionId();
+        $revid2 = $revision2->getRevisionId();
+        if ($revid1 < $revid2) {
+            return $revid1-1;
+        }
+        return $revid2-1;
+    }
+
 }
