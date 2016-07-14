96743a8ec8311012d5d411210720d531149a63bf
Rakesh Verma <rkv160794@gmail.com>
2016-07-14 18:12:15 +0530

    Corrected Indentation, removed comments

--- src/ConflictResolverInterface.php
+++ src/ConflictResolverInterface.php
@@ -5,6 +5,5 @@ namespace Drupal\conflict;
 use Drupal\Core\Entity\RevisionableInterface;
 
 interface ConflictResolverInterface {
-
     public function SimpleMergeResolver(RevisionableInterface $revision1,RevisionableInterface $revision2, RevisionableInterface $revision3);
 }
--- src/SimpleMergeResolver.php
+++ src/SimpleMergeResolver.php
@@ -14,16 +14,8 @@ class SimpleMergeResolver implements ConflictResolverInterface
     public function SimpleMergeResolver(RevisionableInterface $revision1, RevisionableInterface $revision2, RevisionableInterface $revision3)
     {
         $revid1 = $revision1->getRevisionId();
-        $revid2 = $revision2->getRevisionId();
-        $revid3 = $revision3->getRevisionId();
-        if ($revid1 > $revid2 and $revid1 > $revid3) {
-		  return $revid1;
-		}
-	  	elseif ($revid2 > $revid1 and $revid2 > $revid3) {
-		  return $revid2;
-		}
-	  	else {
-		  return $revid3;
-		}
+		$revid2 = $revision2->getRevisionId();
+		$revid3 = $revision2->getRevisionId();
+		return min($revid1, $revid2, $revid3);
     }
 }
