95d9d75c3630f3b4126ce5682ea2394b4772d56f
Rakesh Verma <rkv160794@gmail.com>
2016-07-14 18:36:04 +0530

    Why is this indentation appearing after push?

--- tests/src/Kernel/SimpleMergeTest.php
+++ tests/src/Kernel/SimpleMergeTest.php
@@ -12,77 +12,77 @@ use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
  */
 class SimpleMergeTest extends EntityKernelTestBase {
 
-  protected $entityType;
-  /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  public static $modules = ['entity_test', 'conflict', 'system', 'user'];
+	protected $entityType;
+	/**
+	 * Modules to enable.
+	 *
+	 * @var array
+	 */
+	public static $modules = ['entity_test', 'conflict', 'system', 'user'];
 
-  protected function setUp()
-  {
-	// First setup the needed entity types before installing the views.
-	parent::setUp();
-	$this->installEntitySchema('entity_test_rev');
-  }
+	protected function setUp()
+	{
+		// First setup the needed entity types before installing the views.
+		parent::setUp();
+		$this->installEntitySchema('entity_test_rev');
+	}
 
-  /**
-   * Creates an entity and it's revisions
-   * then performs a simple algorithm to
-   * find common parent of two revisions.
-   */
-  public function testsimpleMergeResolver() {
-	// Creates a new entity
-	$entity = EntityTestRev::create(['name' => 'revision 1']);
-	$entity->save();
-	// Updates it multiple times to create new revisions.
-	$entity->setName('revision 2');
-	$entity->setNewRevision();
-	$entity->save();
-	$entity->setName('revision 3');
-	$entity->setNewRevision();
-	$entity->save();
-	$entity->setName('revision 4');
-	$entity->setNewRevision();
-	$entity->save();
-	$entity->setName('revision 5');
-	$entity->setNewRevision();
-	$entity->save();
-	$entity->setName('revision 6');
-	$entity->setNewRevision();
-	$entity->save();
-	// Load the revisions from database.
-	$revision2 = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision(2);
-	$revision3 = Drupal::entityTypeManager()
-  	  ->getStorage('entity_test_rev')
-	  ->loadRevision(3);
-	$revision4 = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision(4);
-	$revision5 = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision(5);
-	$revision6 = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision(6);
+	/**
+	 * Creates an entity and it's revisions
+	 * then performs a simple algorithm to
+	 * find common parent of two revisions.
+	 */
+	public function testsimpleMergeResolver() {
+		// Creates a new entity
+		$entity = EntityTestRev::create(['name' => 'revision 1']);
+		$entity->save();
+		// Updates it multiple times to create new revisions.
+		$entity->setName('revision 2');
+		$entity->setNewRevision();
+		$entity->save();
+		$entity->setName('revision 3');
+		$entity->setNewRevision();
+		$entity->save();
+		$entity->setName('revision 4');
+		$entity->setNewRevision();
+		$entity->save();
+		$entity->setName('revision 5');
+		$entity->setNewRevision();
+		$entity->save();
+		$entity->setName('revision 6');
+		$entity->setNewRevision();
+		$entity->save();
+		// Load the revisions from database.
+		$revision2 = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision(2);
+		$revision3 = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision(3);
+		$revision4 = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision(4);
+		$revision5 = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision(5);
+		$revision6 = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision(6);
 
-    // This test will fail. It is expected to return "revision 6"
-    $manager = Drupal::service('conflict.simplemerge_resolver');
-	$newest_revision1 = $manager->resolveSimpleMerge($revision2, $revision3, $revision4);
-	$revisionLca = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision($newest_revision1);
-	$this->assertEquals($revisionLca->label(), "revision 4");
+		// This test will fail. It is expected to return "revision 6"
+		$manager = Drupal::service('conflict.simplemerge_resolver');
+		$newest_revision1 = $manager->resolveSimpleMerge($revision2, $revision3, $revision4);
+		$revisionLca = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision($newest_revision1);
+		$this->assertEquals($revisionLca->label(), "revision 4");
 
-	//This test will pass as it returns "revision 6"
-    $newest_revision2 = $manager->resolveSimpleMerge($revision2, $revision3, $revision4);
-    $revisionLca = Drupal::entityTypeManager()
-	  ->getStorage('entity_test_rev')
-	  ->loadRevision($newest_revision2);
-    $this->assertEquals($revisionLca->label(), "revision 6");
+		//This test will pass as it returns "revision 6"
+		$newest_revision2 = $manager->resolveSimpleMerge($revision2, $revision3, $revision4);
+		$revisionLca = Drupal::entityTypeManager()
+			->getStorage('entity_test_rev')
+			->loadRevision($newest_revision2);
+		$this->assertEquals($revisionLca->label(), "revision 6");
 
-  }
+	}
 }
