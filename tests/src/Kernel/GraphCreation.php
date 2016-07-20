ff40601db758c1b54f7ddb76bbc32654746a076a
Rakesh Verma <rkv160794@gmail.com>
2016-07-20 16:33:26 +0530

    file returning all revisions in array

--- tests/src/Kernel/GraphCreation.php
+++ tests/src/Kernel/GraphCreation.php
@@ -1,19 +1,28 @@
 <?php
+function creator($arr, &$storage) {
+    foreach ($arr as $value) {
+        $x = $value['#rev'];
+        $storage[$x] = $x;
+        if (count($value['children'])) {
+            creator($value['children'], $storage);
+        }
+    }
+}
 
-namespace Relaxed\LCA\Test;
-require_once __DIR__."/../vendor/autoload.php";
-use Fhaculty\Graph\Graph as Graph;
-
-
-class GraphCreation
-{
-
-    public function abc() {
-        $array1 = array(
+$array1 = array(
+    array(
+        '#type' => 'rev',
+        '#rev' => 0,
+        '#rev_info' => array(
+            'status' => 'available',
+            'default' => FALSE,
+            'open_rev' => FALSE,
+            'conflict' => FALSE,
+        ),
+        'children' => array(
             array(
                 '#type' => 'rev',
-//                '#uuid' => $uuid,
-                '#rev' => 0,
+                '#rev' => 1,
                 '#rev_info' => array(
                     'status' => 'available',
                     'default' => FALSE,
@@ -23,8 +32,18 @@ class GraphCreation
                 'children' => array(
                     array(
                         '#type' => 'rev',
-//                        '#uuid' => $uuid,
-                        '#rev' => 1,
+                        '#rev' => 2,
+                        '#rev_info' => array(
+                            'status' => 'available',
+                            'default' => FALSE,
+                            'open_rev' => TRUE,
+                            'conflict' => TRUE,
+                        ),
+                        'children' => array(),
+                    ),
+                    array(
+                        '#type' => 'rev',
+                        '#rev' => 3,
                         '#rev_info' => array(
                             'status' => 'available',
                             'default' => FALSE,
@@ -34,75 +53,33 @@ class GraphCreation
                         'children' => array(
                             array(
                                 '#type' => 'rev',
-//                                '#uuid' => $uuid,
-                                '#rev' => 2,
+                                '#rev' => 4,
                                 '#rev_info' => array(
                                     'status' => 'available',
-                                    'default' => FALSE,
+                                    'default' => TRUE,
                                     'open_rev' => TRUE,
-                                    'conflict' => TRUE,
-                                ),
-                                'children' => array(),
-                            ),
-                            array(
-                                '#type' => 'rev',
-//                                '#uuid' => $uuid,
-                                '#rev' => 3,
-                                '#rev_info' => array(
-                                    'status' => 'available',
-                                    'default' => FALSE,
-                                    'open_rev' => FALSE,
                                     'conflict' => FALSE,
                                 ),
-                                'children' => array(
-                                    array(
-                                        '#type' => 'rev',
-//                                        '#uuid' => $uuid,
-                                        '#rev' => 4,
-                                        '#rev_info' => array(
-                                            'status' => 'available',
-                                            'default' => TRUE,
-                                            'open_rev' => TRUE,
-                                            'conflict' => FALSE,
-                                        ),
-                                        'children' => array(),
-                                    )
-                                )
+                                'children' => array(),
                             )
                         )
-                    ),
-                    array(
-                        '#type' => 'rev',
-//                        '#uuid' => $uuid,
-                        '#rev' => 5,
-                        '#rev_info' => array(
-                            'status' => 'available',
-                            'default' => FALSE,
-                            'open_rev' => TRUE,
-                            'conflict' => TRUE,
-                        ),
-                        'children' => array(),
                     )
                 )
+            ),
+            array(
+                '#type' => 'rev',
+                '#rev' => 5,
+                '#rev_info' => array(
+                    'status' => 'available',
+                    'default' => FALSE,
+                    'open_rev' => TRUE,
+                    'conflict' => TRUE,
+                ),
+                'children' => array(),
             )
-        );
-//        print_r($array1);
-        $x = $this->creator($array1);
-    }
-
-    public function creator($arr) {
-        $vertices = [];
-        $graph = new Graph();
-        foreach ($arr as $value) {
-            array_push($vertices,$value['#rev']);
-            if (count($value['children'])) {
-                $this->creator($value['children']);
-            }
-        }
-        print_r($vertices);
-        return $vertices;
-    }
-}
-
-$a = new GraphCreation();
-$a->abc();
+        )
+    )
+);
+$storage = array();
+creator($array1, $storage);
+print_r($storage);
